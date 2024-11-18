fd4054c1620289da965d73281a2affc5f214e871
yvonnkr <yvonnkr86@gmail.com>
2024-11-18 00:48:44 +0000

    implement testimonials section animation

--- src/sections/TestimonialsSection.tsx
+++ src/sections/TestimonialsSection.tsx
@@ -1,4 +1,5 @@
 import {twMerge} from "tailwind-merge";
+import {motion} from "framer-motion";
 
 const testimonials = [
     {
@@ -28,8 +29,25 @@ export const TestimonialsSection = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 md:gap-8 lg:gap-12">
                     {testimonials.map((testimonial, testimonialIndex) => (
-                        <blockquote key={testimonialIndex}
-                                    className={twMerge(testimonialIndex === 2 && "md:hidden lg:block")}
+                        <motion.blockquote
+                            key={testimonialIndex}
+                            className={twMerge(testimonialIndex === 2 && "md:hidden lg:block")}
+                            initial={{
+                                opacity: 0,
+                                y: 24
+                            }}
+                            whileInView={{
+                                opacity: 1,
+                                y: 0
+                            }}
+                            viewport={{
+                                once: true,
+                            }}
+                            transition={{
+                                delay: testimonialIndex * 0.5,
+                                ease:"easeInOut",
+                                duration: 1
+                            }}
                         >
                             <p className="font-heading text-3xl lg:text-4xl font-black">
                                 &ldquo;{testimonial.text}&rdquo;
@@ -54,7 +72,7 @@ export const TestimonialsSection = () => {
                                     </div>
                                 </div>
                             </cite>
-                        </blockquote>
+                        </motion.blockquote>
                     ))}
                 </div>
 
