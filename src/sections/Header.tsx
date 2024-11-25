694e662431cfe44a549d7a4e75f3db536ad223f3
yvonnkr <yvonnkr86@gmail.com>
2024-11-25 15:04:13 +0000

    implement blog article page

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,37 +1,37 @@
-import {CutCornerButton} from "../components/CutCornerButton.tsx";
-import {Hexagon} from "../components/Hexagon.tsx";
-import {useEffect, useState} from "react";
-import {twMerge} from "tailwind-merge";
-import {AnimatePresence, motion} from "framer-motion";
+import { CutCornerButton }         from "../components/CutCornerButton.tsx";
+import { Hexagon }                 from "../components/Hexagon.tsx";
+import { useEffect, useState }     from "react";
+import { twMerge }                 from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
 
 const navLinks = [
     {
         title: "Home",
-        href: "/"
+        href: "/",
     },
     {
         title: "Blog",
-        href: "/blog"
+        href: "/blog",
     },
     {
         title: "Careers",
-        href: "/careers"
+        href: "/careers",
     },
     {
         title: "Contact",
-        href: "/contact"
+        href: "/contact",
     },
-]
+];
 
 export const HeaderSection = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         //stop background body from scrolling when navbar is open
-        if (isOpen){
-            document.body.style.overflow = "hidden"
-        }else {
-            document.body.style.overflow = "auto"
+        if (isOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
         }
 
     }, [isOpen]);
@@ -43,23 +43,25 @@ export const HeaderSection = () => {
                 <div className="container">
                     <div className="flex justify-between items-center h-24 md:h-28">
                         <div>
-                            <img src="/assets/images/logo.svg" alt="Blockforge Logo"/>
+                            <img src="/assets/images/logo.svg" alt="Blockforge Logo" />
                         </div>
                         <div className="flex gap-4 items-center">
-                            <CutCornerButton className="hidden md:inline-flex">
-                                Get Started
-                            </CutCornerButton>
+                            <a href="/">
+                                <CutCornerButton className="hidden md:inline-flex">
+                                    Get Started
+                                </CutCornerButton>
+                            </a>
                             <div className="size-10 relative" onClick={() => setIsOpen((curr) => !curr)}>
                                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2">
                                     <div className={twMerge(
                                         "w-5 h-0.5 bg-zinc-300 -translate-y-1 transition-all duration-500",
-                                        isOpen && "translate-y-0 rotate-45"
+                                        isOpen && "translate-y-0 rotate-45",
                                     )}></div>
                                 </div>
                                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2">
                                     <div className={twMerge(
                                         "w-5 h-0.5 bg-zinc-300 translate-y-1 transition-all duration-500",
-                                        isOpen && "translate-y-0 -rotate-45"
+                                        isOpen && "translate-y-0 -rotate-45",
                                     )}></div>
                                 </div>
                             </div>
@@ -72,29 +74,29 @@ export const HeaderSection = () => {
                 {isOpen && (
                     <motion.div
                         className="fixed size-full top-0 left-0 z-30 bg-zinc-900"
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                        transition={{duration: 0.5}}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.5 }}
                     >
                         <div className="absolute inset-2 rounded-md bg-zinc-800 mt-24 md:mt-28 z-0">
                             {/*background-start*/}
                             <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
-                                <Hexagon size={700}/>
+                                <Hexagon size={700} />
                             </div>
                             <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
-                                <Hexagon size={1100}/>
+                                <Hexagon size={1100} />
                             </div>
                             {/*background-end*/}
 
                             <div className="h-full flex justify-center items-center">
                                 <nav className="flex flex-col items-center gap-12 md:gap-16">
-                                    {navLinks.map(({title, href}, index) => (
+                                    {navLinks.map(({ title, href }, index) => (
                                         <motion.a href={href}
                                                   key={title}
-                                                  initial={{opacity: 0, y: 20}}
-                                                  animate={{opacity: 1, y: 0}}
-                                                  transition={{duration: 0.5, ease: "linear", delay: 0.25 * index}}
+                                                  initial={{ opacity: 0, y: 20 }}
+                                                  animate={{ opacity: 1, y: 0 }}
+                                                  transition={{ duration: 0.5, ease: "linear", delay: 0.25 * index }}
                                         >
                                             <span
                                                 className="text-4xl md:text-5xl lg:text-6xl font-heading font-black text-zinc-500 hover:text-zinc-300 transition duration-300"
@@ -110,5 +112,5 @@ export const HeaderSection = () => {
                 )}
             </AnimatePresence>
         </>
-    )
-}
+    );
+};
